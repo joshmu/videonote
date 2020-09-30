84223acb129b5a2898adcaf0bb4fcd3638f093a5
Josh Mu <mu@joshmu.com>
2020-09-30 15:45:07 +1000

    :sparkles: trash for projects

--- src/components/Modals/ProjectsModal.js
+++ src/components/Modals/ProjectsModal.js
@@ -1,19 +1,29 @@
+import { useState } from 'react'
 import { useGlobalContext } from '../../context/globalContext'
 import Select from '../shared/Select'
 import { ModalContainer, ModalHeader } from './Modal'
+import { ImBin2 as TrashIcon } from 'react-icons/im'
+import { motion } from 'framer-motion'
 
 export default function ProjectsModal({ toggle: toggleModal }) {
   const { projects, switchProject, removeProject } = useGlobalContext()
+  const [mousingOver, setMousingOver] = useState(null)
 
   const handleSelection = id => {
     switchProject(id)
     toggleModal()
   }
 
-  const handleRightClick = (e, id) => {
-    e.preventDefault()
-    removeProject(id)
-    toggleModal()
+  const handleRemoveProject = ({ title, id }) => {
+    const answer = window.confirm(
+      `Are you sure you want to delete the project: ${title}`
+    )
+    if (answer) removeProject(id)
+  }
+
+  const handleMousePosition = (inside, project) => {
+    if (inside) setMousingOver(project.id)
+    if (!inside) setMousingOver(null)
   }
 
   return (
@@ -24,11 +34,26 @@ export default function ProjectsModal({ toggle: toggleModal }) {
         {projects.map(project => (
           <Select
             key={project.id}
-            onClick={() => handleSelection(project.id)}
-            onContextMenu={e => handleRightClick(e, project.id)}
+            onMouseEnter={() => handleMousePosition(true, project)}
+            onMouseLeave={() => handleMousePosition(false, project)}
           >
-            <span className='text-sm capitalize'>{project.title} </span>
-            <span className='text-xs'>({project.todos.length} notes)</span>
+            <div onClick={() => handleSelection(project.id)} className='flex-1'>
+              <span className='text-sm capitalize'>{project.title} </span>
+              <span className='text-xs'>({project.todos.length} notes)</span>
+            </div>
+            {mousingOver === project.id && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                onClick={() => {
+                  handleRemoveProject(project)
+                }}
+                className='transition-colors duration-300 ease-in-out cursor-pointer hover:text-highlight-400 text-themeText2'
+              >
+                <TrashIcon />
+              </motion.div>
+            )}
           </Select>
         ))}
       </div>
