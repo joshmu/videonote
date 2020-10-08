4a4e2cb8df5c2829394e701201c94ecdb33dc0bd
Josh Mu <mu@joshmu.com>
2020-10-08 17:12:29 +1000

    :sparkles: share projects

--- src/components/Modals/CurrentProjectModal.js
+++ src/components/Modals/CurrentProjectModal.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useGlobalContext } from '../../context/globalContext'
 import LocalVideoForm from '../LocalVideo/LocalVideoForm'
+import { motion } from 'framer-motion'
 import {
   ModalContainer,
   ModalForm,
@@ -9,12 +10,15 @@ import {
   ModalInput,
   ModalPrimaryBtn,
 } from './Modal'
+import { ToggleInput } from '../shared/Toggle'
+import { useNotificationContext } from '../../context/notificationContext'
 
 export default function CurrentProjectModal({
   toggle: toggleModal,
   motionKey,
 }) {
   const { updateProject, project } = useGlobalContext()
+  const { addAlert } = useNotificationContext()
   const [state, setState] = useState(null)
 
   useEffect(() => {
@@ -40,6 +44,32 @@ export default function CurrentProjectModal({
     if (typeof url !== 'string' || url.length === 0) return
     const updated = { ...state, src: url }
     setState(updated)
+  }
+
+  const handleShare = () => {
+    // toggle
+    const updated = !state.isPrivate
+    // create new project state
+    const updatedState = { ...state, isPrivate: updated }
+    setState(updatedState)
+    // as this is important we will send to the server rather than wait for user to update manually
+    updateProject(updatedState)
+  }
+
+  const handleShareUrlClick = () => {
+    if (state.isPrivate) return
+    const url = `https://videonote.app/vn/${project._id}`
+    // copy to clipboard
+    navigator.clipboard.writeText(url).then(
+      function () {
+        /* clipboard successfully set */
+        addAlert({ type: 'info', msg: `Copied to clipboard! ${url}` })
+      },
+      function () {
+        /* clipboard write failed */
+        console.log('clipboard copy failed')
+      }
+    )
   }
 
   return (
@@ -68,6 +98,27 @@ export default function CurrentProjectModal({
               />
 
               <LocalVideoForm handleVideoSrc={handleVideoSrc} />
+
+              <div className='relative mt-2'>
+                <ToggleInput
+                  title={
+                    state.isPrivate
+                      ? 'Share this project?'
+                      : 'Sharing this project!'
+                  }
+                  state={!state.isPrivate}
+                  onClick={handleShare}
+                />
+                {!state.isPrivate && (
+                  <motion.span
+                    whileHover={{ scale: 0.95 }}
+                    onClick={handleShareUrlClick}
+                    className='absolute italic cursor-pointer -bottom-full text-highlight-400'
+                  >
+                    videonote.app/vn/{project._id}
+                  </motion.span>
+                )}
+              </div>
 
               <div></div>
               <ModalPrimaryBtn onClick={handleUpdate}>Update</ModalPrimaryBtn>
