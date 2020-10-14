99e842b41f18c7cb28d39acd662cbe85b5d96ad0
Josh Mu <mu@joshmu.com>
2020-10-14 15:18:39 +1000

    :zap: ui options dropdown

--- src/components/Sidebar/OptionsDropdown/OptionsDropdown.js
+++ src/components/Sidebar/OptionsDropdown/OptionsDropdown.js
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import Link from 'next/link'
 import Cookie from 'universal-cookie'
 
@@ -46,80 +47,100 @@ const OptionsDropdown = ({ open }) => {
     copyToClipboard()
   }
 
+  const variants = {
+    initial: { opacity: 0 },
+    animate: {
+      opacity: 1,
+      transition: {
+        duration: 0.2,
+      },
+    },
+    exit: { opacity: 0, transition: { duration: 0.2 } },
+  }
+
   return (
     <div>
-      {open && (
-        <div
-          onMouseLeave={handleMouseLeave}
-          className='absolute right-0 z-40 w-48 py-2 text-sm capitalize transition-colors duration-300 ease-in-out border rounded-sm shadow-xl bg-themeBg'
-        >
-          {project && admin && (
-            <Select onClick={handleClick} data-modal='current'>
-              <div className='uppercase text-highlight-400'>
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            key='optionsDropdown'
+            initial='inital'
+            animate='animate'
+            exit='exit'
+            variants={variants}
+            onMouseLeave={handleMouseLeave}
+            className='absolute right-0 z-40 w-48 py-2 text-sm capitalize transition-colors duration-300 ease-in-out border rounded-sm shadow-xl bg-themeBg'
+          >
+            {project && admin && (
+              <Select onClick={handleClick} data-modal='current'>
+                <div className='uppercase text-highlight-400'>
+                  {project.title}
+                </div>
+              </Select>
+            )}
+            {!admin && (
+              <div className='flex items-center px-4 py-2 uppercase text-highlight-400'>
                 {project.title}
               </div>
+            )}
+
+            {admin && project && (
+              <Select onClick={handleShareProject}>Share Project</Select>
+            )}
+
+            {admin && (
+              <Select
+                onClick={handleClick}
+                data-modal='create'
+                animate={
+                  projects.length === 0
+                    ? 'animate-pulse text-highlight-400'
+                    : ''
+                }
+              >
+                create new
+              </Select>
+            )}
+
+            {admin && projects.length > 0 && (
+              <Select onClick={handleClick} data-modal='projects'>
+                projects
+              </Select>
+            )}
+
+            <Select onClick={handleClick} data-modal='settings'>
+              settings
             </Select>
-          )}
-          {!admin && (
-            <div className='flex items-center px-4 py-2 uppercase text-highlight-400'>
-              {project.title}
-            </div>
-          )}
 
-          {admin && project && (
-            <Select onClick={handleShareProject}>Share Project</Select>
-          )}
+            {admin && (
+              <Select onClick={handleClick} data-modal='user'>
+                account
+              </Select>
+            )}
+            {admin && (
+              <Link href='/hello' passHref>
+                <a>
+                  <Select onClick={handleSignOutClick}>Sign Out</Select>
+                </a>
+              </Link>
+            )}
 
-          {admin && (
-            <Select
-              onClick={handleClick}
-              data-modal='create'
-              animate={
-                projects.length === 0 ? 'animate-pulse text-highlight-400' : ''
-              }
-            >
-              create new
+            <Select onClick={handleThemeToggleClick}>
+              <div className='relative flex text-md'>
+                <ThemeToggle />
+              </div>
             </Select>
-          )}
 
-          {admin && projects.length > 0 && (
-            <Select onClick={handleClick} data-modal='projects'>
-              projects
+            <Select onClick={handleClick} data-modal='help'>
+              Help
             </Select>
-          )}
 
-          <Select onClick={handleClick} data-modal='settings'>
-            settings
-          </Select>
-
-          {admin && (
-            <Select onClick={handleClick} data-modal='user'>
-              account
+            <Select onClick={handleClick} data-modal='about'>
+              About VideoNote
             </Select>
-          )}
-          {admin && (
-            <Link href='/hello' passHref>
-              <a>
-                <Select onClick={handleSignOutClick}>Sign Out</Select>
-              </a>
-            </Link>
-          )}
-
-          <Select onClick={handleThemeToggleClick}>
-            <div className='relative flex text-md'>
-              <ThemeToggle />
-            </div>
-          </Select>
-
-          <Select onClick={handleClick} data-modal='help'>
-            Help
-          </Select>
-
-          <Select onClick={handleClick} data-modal='about'>
-            About VideoNote
-          </Select>
-        </div>
-      )}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
--- src/components/shared/Select/Select.js
+++ src/components/shared/Select/Select.js
@@ -1,4 +1,9 @@
-export default function Select({ children, padding = 'px-4 py-2', animate='', ...props }) {
+export default function Select({
+  children,
+  padding = 'px-4 py-2',
+  animate = '',
+  ...props
+}) {
   return (
     <div
       className={`${padding} ${animate} flex items-center cursor-pointer text-themeText hover:bg-blue-800 hover:text-white`}
