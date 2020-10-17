b5e6f2ea6335f6d69b9886d93d3c29f4de8bee3f
Josh Mu <mu@joshmu.com>
2020-10-17 18:31:10 +1000

    :books: todo

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import { AnimatePresence } from 'framer-motion'
 import absoluteUrl from 'next-absolute-url'
 import Cookies from 'universal-cookie'
 
@@ -24,6 +23,8 @@ import { fetcher } from '@/utils/clientHelpers'
 
 // todo: think about extending landing page
 // todo: insert an accept cookies msg? or should we avoid cookies so we don't need this?
+// todo: offer restrictation on sharing to avoid other users adding their own notes (editable) (DB)
+// todo: offer public url however password protected (DB)
 
 // todo: create docker instance for demo purposes?
 
@@ -39,10 +40,12 @@ import { fetcher } from '@/utils/clientHelpers'
 // todo: more user friendly sharing? project title instead? (and when clash then add random suffix?) or half id? unique project title specifically for sharing?
 // todo: think about mobile variant
 
+// todo: handle an audio file instead of a video file?
+// todo: naming scheme which can cover using audio?
+
 // todo: provide option for user to cancel notification messages? longer for project listing?
 // todo: changing password in user account modal produces error page
 // todo: understand basic ux rules
-// todo: restrict playback offset to only handle negative numbers
 // todo: email account cannot handle full stops?  (is this normalisation occurring on the backend?)
 // todo: confirmation modal for all 'delete' actions (whenever action is irreversible)
 // todo: undo action for deleted projects and notes?
@@ -52,6 +55,26 @@ import { fetcher } from '@/utils/clientHelpers'
 // todo: detect new user on signin and defer to register modal
 // todo: add about information to /hello
 // todo: create new project needs correct validation (compare to currentProjectModal)
+// todo: multi users per project > check project usersIds before removing a project from db (DB: primary owner, who can also have extended control?) Multi users will have the project in their collection
+// todo: playback speed in settings and smartcontrol shortcut with '<' '>' (DB)
+// todo: sidebarWidth specification via settings
+// todo: initialSidebarWidth based on user screen size
+// todo: mark the video time line where notes are placed
+// todo: when selecting timestamp edit then type or drag video playhead to change
+// todo: offer ability to reply to a note? (DB)
+// todo: project selection includes video thumbnail? (only for url based videos)
+// todo: multiple export formats (excel, pdf, word, local to import back in to vn?, reminders/notes ios checklist?)
+// todo: utilize shared storage? google drive? should we remove our own db dependency entirely!? then there would be very little overhead
+// todo: notes have ability to add canvas mark?
+// todo: realtime websockets data layer? users in the same project can unify their data, control video in realtime with each other? open video call? see canvas drawings or atleast each others cursor?
+// todo: project last updated in the listing
+
+// todo: mobile can still be used and when not sure of a note then select and it plays the video at the position required. (workflow of flicking between)
+// todo: demo video = the videonote workflow... (explain the ease of note taking whilst video is running by having a playback offset, otherwise the simplicity of toggle and note flow)
+
+// todo: can we use localStorage to save screenshots of video timestamp positions? use this in export feature
+
+// todo: browser cross compatability of video files...
 
 export default function Main({ serverData }) {
   return (
@@ -60,7 +83,7 @@ export default function Main({ serverData }) {
         <TodoProvider>
           <Layout>
             <div className='flex flex-col w-full h-screen overflow-hidden'>
-              {/* <div className='text-3xl'>navbar</div> */}
+              {/* <div className='text-3xl'>header</div> */}
 
               <div className='flex flex-1 w-full h-full'>
                 <VideoPlayer />
