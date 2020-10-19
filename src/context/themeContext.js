dc598784e6cba500501d527ee5e7c5061a645dfd
Josh Mu <mu@joshmu.com>
2020-10-19 10:40:55 +1000

    :zap: constant for localstorage theme key

--- src/context/themeContext.js
+++ src/context/themeContext.js
@@ -5,6 +5,7 @@ const themeContext = createContext({
   toggleTheme: () => {},
 })
 
+const LOCALSTORAGE_KEY = 'vn:theme'
 const THEME_TYPES = {
   dark: 'theme-dark',
   light: 'theme-light',
@@ -17,14 +18,14 @@ export function ThemeProvider(props) {
   // initial theme
   useEffect(() => {
     // get locally stored theme
-    let savedTheme = window.localStorage.getItem('theme')
+    let savedTheme = window.localStorage.getItem(LOCALSTORAGE_KEY)
 
     // validation check
     if (!Object.keys(THEME_TYPES).includes(savedTheme)) savedTheme = null
 
     // if we don't have local stored then lets set it
     if (!savedTheme) {
-      window.localStorage.setItem('theme', theme)
+      window.localStorage.setItem(LOCALSTORAGE_KEY, theme)
     }
 
     // if we have a local stored theme then let's set it
@@ -54,7 +55,7 @@ export function ThemeProvider(props) {
     if (!themeIdList.includes(newThemeId)) newThemeId = themeIdList[0]
 
     setTheme(newThemeId)
-    window.localStorage.setItem('theme', newThemeId)
+    window.localStorage.setItem(LOCALSTORAGE_KEY, newThemeId)
   }
 
   const value = {
