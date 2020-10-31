7c69a5c7cd7e421c67b58eeffd1445a8babc377c
Josh Mu <mu@joshmu.com>
2020-10-31 16:30:23 +1000

    :books: loader credit

--- src/components/shared/Loader/Loader.js
+++ src/components/shared/Loader/Loader.js
@@ -1,12 +1,13 @@
 import styles from './LoaderStyle.module.scss'
 
+// @see https://codepen.io/martinvd
 const Loader = () => {
   return (
-      <div className={styles.loader}>
-        <div className={`${styles.inner} ${styles.one}`}></div>
-        <div className={`${styles.inner} ${styles.two}`}></div>
-        <div className={`${styles.inner} ${styles.three}`}></div>
-      </div>
+    <div className={styles.loader}>
+      <div className={`${styles.inner} ${styles.one}`}></div>
+      <div className={`${styles.inner} ${styles.two}`}></div>
+      <div className={`${styles.inner} ${styles.three}`}></div>
+    </div>
   )
 }
 
