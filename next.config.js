c80aaf85cd888327244f8804e3ad2e2ed442ff16
Josh Mu <mu@joshmu.com>
2020-12-11 12:39:39 +1000

    chore: 🐛 clean up nextjs config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
-/**
- * @path /next.config.js
- *
- * @project videonote
- * @file next.config.js
- *
- * @author Josh Mu <hello@joshmu.dev>
- * @created Friday, 11th December 2020
- * @modified Friday, 11th December 2020 11:25:38 am
- * @copyright © 2020 - 2020 MU
- */
-
 module.exports = {
   images: {
     domains: ['joshmu.s3-ap-southeast-2.amazonaws.com'],
